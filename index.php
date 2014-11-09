815f2e9f284e1191dae86bbe2c1e3672ed20e63c
huszara2 <huszara2@tcnj.edu>
2014-11-09 17:21:02 -0500

    maybe the echo?

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
 <?php
 
 		
-	print 'Pargameters';
+	echo 'Pargameters';
 	$gameTest=new Game('Dominion',' ',10,100,30,50,'Deck building',2,5);
-	print $gameTest->getTitle();
+	echo $gameTest->getTitle();
 	$gameTest->setType('tabletop');
-	print $gameTest->getType();
+	echo $gameTest->getType();
 	
 ?>
 
