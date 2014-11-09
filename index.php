8457d8e1edbb2a4805ceb11bda0357157bd55670
huszara2 <huszara2@tcnj.edu>
2014-11-09 17:27:00 -0500

    include

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <body>
-
+<?php include 'game.php';?>
 		
 <?php
 
-		
+	echo 'Pargameters';	
 	
 	$gameTest = new Game('Dominion','U',10,100,30,50,'Deck building',2,5);
 	echo $gameTest->getTitle();
 	$gameTest->setType('tabletop');
 	echo $gameTest->getType();
-	echo 'Pargameters';
+
 ?>
 
 		
