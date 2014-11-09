e0c3f3d1774803db514505698b6bc0c70f731f2c
huszara2 <huszara2@tcnj.edu>
2014-11-09 17:15:58 -0500

    fixed quotes

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
 
 		
 	print 'Pargameters';
-	$gameTest=new Game("Dominion"," ",10,100,30,50,"Deck building",2,5);
+	$gameTest=new Game('Dominion',' ',10,100,30,50,'Deck building',2,5);
 	print $gameTest->getTitle();
-	$gameTest->setType("tabletop");
+	$gameTest->setType('tabletop');
 	print $gameTest->getType();
 	
 ?>
