eea11e20c6d22d59bd8cc08380a5858a3b103e23
huszara2 <huszara2@tcnj.edu>
2014-11-09 17:13:01 -0500

    arrows

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
 
 		
 	print 'Pargameters';
-	/*$gameTest=new Game("Dominion"," ",10,100,30,50,"Deck building",2,5);
-	print $gameTest.getTitle();
-	$gameTest.setType("tabletop");
-	print $gameTest.getType();
-	*/
+	$gameTest=new Game("Dominion"," ",10,100,30,50,"Deck building",2,5);
+	print $gameTest->getTitle();
+	$gameTest->setType("tabletop");
+	print $gameTest->getType();
+	
 ?>
 
 		
