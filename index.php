9269653d10cbd1c29471c05ea5ce160dad3818c2
huszara2 <huszara2@tcnj.edu>
2014-11-09 17:09:57 -0500

    fixed typo

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <body>
 
 		
-<?PHP
+<?php
 
 		
 	print 'Pargameters';
 	$gameTest=new Game("Dominion"," ",10,100,30,50,"Deck building",2,5);
 	print $gameTest.getTitle();
 	$gameTest.setType("tabletop");
-	$gameTest.getType();
+	print $gameTest.getType();
 		
 ?>
 
