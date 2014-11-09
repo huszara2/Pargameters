14f0018ff2fbc51940a3678fb597a549cbcf5fdd
huszara2 <huszara2@tcnj.edu>
2014-11-09 17:06:31 -0500

    Test for game object

--- index.php
+++ index.php
@@ -4,8 +4,11 @@
 <?PHP
 
 		
-print 'Pargameters';
-
+	print 'Pargameters';
+	$gameTest=new Game("Dominion"," ",10,100,30,50,"Deck building",2,5);
+	print $gameTest.getTitle();
+	$gameTest.setType("tabletop");
+	$gameTest.getType();
 		
 ?>
 
