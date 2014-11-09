fc784e713e68d3e54fea43016f0141eae3b5910b
huszara2 <huszara2@tcnj.edu>
2014-11-09 16:59:01 -0500

    Game object

---
+++ game.php
@@ -0,0 +1,52 @@
+/*
+ *Alana Huszar
+ *CSC 415
+ *Independent Project
+/*
+
+/* Games object
+*/
+<?php
+	class Game{
+		private $title;
+		private $type;
+		private $minAge;
+		private $maxAge;
+		private $minTime;
+		private $maxTime;
+		private $genre;
+		private $minPlayers;
+		private $maxPlayers;
+
+		function __construct($title,$type,$minAge,$maxAge,$minTime,$maxTime,$genre,$minPlayers,$maxPlayers) {
+			$this->title=$title;
+			$this->type=$type;
+			$this->minAge=$minAge;
+			$this->maxAge=$maxAge;
+			$this->minTime=$minTime;
+			$this->maxTime=$maxTime;
+			$this->genre=$genre;
+			$this->minPlayers=$minPlayers;
+			$this->maxPlayers=$maxPlayers;
+		}
+		
+		public function getTitle() {
+			return $title;
+		}
+		
+		public function setTitle($newTitle) {
+			$title=$newTitle;
+		}
+		
+		public function getType() {
+			return $type;
+		}
+		
+		public function setType($newType) {
+			$type=$newType;
+		}
+
+
+	}
+?>
+	
