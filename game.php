c8647238cefd14cfcb2da7241e24decb1f68285e
huszara2 <huszara2@tcnj.edu>
2014-11-15 18:57:28 -0500

    myCollection test

--- game.php
+++ game.php
@@ -18,7 +18,6 @@
 		private $genre;
 		private $minPlayers;
 		private $maxPlayers;
-		private $compareNumber=0;
 
 		public function __construct($title,$type,$minAge,$maxAge,$minTime,$maxTime,$genre,$minPlayers,$maxPlayers) {
 			$this->title=$title;
@@ -90,7 +89,7 @@
 		}
 		
 		public function timeRange() {
-			return $this->minTime() . "-" . this->maxTime();
+			return $this->minTime() . "-" . $this->maxTime();
 		}
 				
 		public function getGenre() {
@@ -119,27 +118,6 @@
 		
 		public function __toString() {
 				return $this->title . ", " . $this->type . ", " . $this->minAge . "-" . $this->maxAge . " years, " . $this->minTime . "-" . $this->maxTime . " minutes, " . $this->genre . ", " . $this->minPlayers . "-" . $this->maxPlayers . " players.";
-		}
-		
-		public static function compType($compNum) {
-			$this->compareNumber=$compNum;
-		}
-		
-		public static function cmp($gameA, $gameB) {
-			if($this->compareNumber==0) { //title
-				return strcmp($gameA->getTitle(),$gameB->getTitle());
-			}
-			/*if($this->compareNumber==1) { //type
-				return strcmp($gameA->getType(),$gameB->getType());
-			}
-			if($this->compareNumber==2) { //minAge
-				if($gameA->getMinAge() == $gameB->getMinAge()) {
-					return 0;
-				}
-				else {
-					return($gameA->getMinAge() < $gameB->getMinAge()
-			*/
-		
 		}
 		
 		
--- index.php
+++ index.php
@@ -1,18 +1,27 @@
 <body>
-<?php include_once("game.php");?>
+<?php include_once("game.php");
+	  include_once("collection.php")?>
 		
 <?php
 
 	echo 'Pargameters';	
 	
-	$gameTest = new Game('Dominion','tabletop',10,100,30,50,'Deck building',2,5);
+	$gameTest1 = new Game('Dominion','tabletop',10,100,30,50,'Deck building',2,5);
+	$gameTest2 = new Game('Superfight','tabletop',6,100,20,40,'Storytelling',3,10);
+	$gameTest3 = new Game('MarioKart 8','video',5,100,3,60,'Racing',1,8);
+	
+	$list=new SplDoublyLinkedList();
+	$list->push($gameTest1);
+	$list->push($gameTest2);
+	$list->push($gameTest3);
+	
+	$myCollection = new Collection($list);
+	
 	echo "\n";
-	echo $gameTest;
-	echo $gameTest->ageRange();
-	echo "\n";
-	echo $gameTest->__toString();
-	echo $gameTest->timeRange();
-
+	echo $gameTest2;
+	echo $gameTest2->timeRange();
+	echo $myCollection;
+	
 
 ?>
 
