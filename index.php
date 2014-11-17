84b2db453aad1913b68a23a94946653658638267
huszara2 <huszara2@tcnj.edu>
2014-11-17 14:06:19 -0500

    Using divs

--- index.php
+++ index.php
@@ -4,7 +4,14 @@
 </head>
 <body>
 <form action="results.php" method="post">
-<input type="text" name="genre" value="Genre" />
+	<div>
+		<label for "genre">Genre:</label>
+		<input type="text" id="genre" name="genre" />
+	</div>
+	<div>
+		<label for "genre">Genre:</label>
+		<input type="text" id="genre" name="genre" />
+	</div>
 
 <input type="CHECKBOX" name="type_Video" checked>Video Game
 <input type="CHECKBOX" name="type_TT" checked>Tabletop
@@ -14,32 +21,5 @@
 
 <input type="submit" name="submit_button" value="Find a game!" />
 
-</form>
-
-
-<?php include_once("game.php");?>
-		
-<?php
-	
-	$gameTest1 = new Game('Dominion','Tabletop',10,100,30,50,'Deck building',2,5);
-	$gameTest2 = new Game('Superfight','Tabletop',6,100,20,40,'Storytelling',3,10);
-	$gameTest3 = new Game('MarioKart 8','Video',5,100,3,60,'Racing',1,8);
-	
-	$list=new SplDoublyLinkedList();
-	$list->push($gameTest1);
-	$list->push($gameTest2);
-	$list->push($gameTest3);
-	
-	
-	echo "\n";
-	
-	$list->rewind();
-	while($list->valid()) {
-		echo $list->current() . "\n";
-		$list->next();
-	}
-
-?>
-
-		
+</form>	
 </body>
---
+++ results.php
@@ -0,0 +1,25 @@
+<body>
+<?php include_once("game.php");?>
+		
+<?php
+	
+	$gameTest1 = new Game('Dominion','Tabletop',10,100,30,50,'Deck building',2,5);
+	$gameTest2 = new Game('Superfight','Tabletop',6,100,20,40,'Storytelling',3,10);
+	$gameTest3 = new Game('MarioKart 8','Video',5,100,3,60,'Racing',1,8);
+	
+	$list=new SplDoublyLinkedList();
+	$list->push($gameTest1);
+	$list->push($gameTest2);
+	$list->push($gameTest3);
+	
+	
+	echo "\n";
+	
+	$list->rewind();
+	while($list->valid()) {
+		echo $list->current() . "\n";
+		$list->next();
+	}
+
+?>
+</body>
