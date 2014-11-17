e316c26a696eed5245444bd76594e10503de7566
huszara2 <huszara2@tcnj.edu>
2014-11-17 15:13:55 -0500

    Adding title back in

--- index.php
+++ index.php
@@ -5,6 +5,12 @@
 <body>
 <form action="results.php" method="post">
 	<div>
+		<?php echo "Pargameters";
+			echo "<br>/n</br>";
+			echo "What would you like to play?";
+		?>
+	</div>
+	<div>
 		<label for "genre">Genre:</label>
 		<input type="text" id="genre"/>
 	</div>
@@ -16,10 +22,20 @@
 	<div>
 		<label for "age">Age Range:</label>
 		<input type="text" name="minAge" value="Minimum Age" />
-		<input type="text" name="maxAge" value="Maximum Age" />
+		<input type="text" name="maxAge" value="Maximum Age" />-
 		<input type="CHECKBOX" name="eighteenPlus" checked>18+
+		<?php echo "years";?>
+	</div>
+	<div>
+		<label for "time">Time:</label>
+		<input type="text" name="minTime" value="Minimum Time" />
+		<input type="text" name="maxTime" value="Maximum Time" />-
+		<input type="CHECKBOX" name="twoPlus" checked>120+
+		<?php echo "minutes";?>
 	</div>
 
+	
+	
 	<div>
 		<input type="submit" name="submit_button" value="Find a game!" />
 	</div>
