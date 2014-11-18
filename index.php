5b6ed11cc966a2a4f8e749dd0d8e178820afaafc
huszara2 <huszara2@tcnj.edu>
2014-11-18 12:08:52 -0500

    Added players

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
 <form action="results.php" method="post">
 	<div>
 		<?php echo "Pargameters";
-			echo "<br>/n</br>";
 			echo "What would you like to play?";
 		?>
 	</div>
@@ -21,17 +20,21 @@
 	</div>
 	<div>
 		<label for "age">Age Range:</label>
-		<input type="text" name="minAge" value="Minimum Age" />
-		<input type="text" name="maxAge" value="Maximum Age" />-
+		<input type="text" name="minAge" value="0" /> -
+		<input type="text" name="maxAge" value="" />
 		<input type="CHECKBOX" name="eighteenPlus" checked>18+
 		<?php echo "years";?>
 	</div>
 	<div>
 		<label for "time">Time:</label>
-		<input type="text" name="minTime" value="Minimum Time" />
-		<input type="text" name="maxTime" value="Maximum Time" />-
+		<input type="text" name="minTime" value="0" /> -
+		<input type="text" name="maxTime" value="" />
 		<input type="CHECKBOX" name="twoPlus" checked>120+
 		<?php echo "minutes";?>
+	</div>
+	<div>
+		<label for "players">Players:</label>
+		<input type="text" id="players" value="4"/>
 	</div>
 
 	
