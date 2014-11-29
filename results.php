cf7743752959c63d1796a8637dfb2d0daed806ac
huszara2 <huszara2@tcnj.edu>
2014-11-29 09:56:26 -0500

    From file

--- results.php
+++ results.php
@@ -15,6 +15,19 @@
 	
 	$errorMessage="";
 	
+	$row = 1;
+	if (($handle = fopen("user1.csv", "r")) !== FALSE) {
+		while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+			$num = count($data);
+			echo "<p> $num fields in line $row: <br /></p>\n";
+			$row++;
+			for ($c=0; $c < $num; $c++) {
+				echo $data[$c] . "<br />\n";
+			}
+		}
+		fclose($handle);
+	}	
+	
 	/* Games listed
 	*/
 	$list=new SplDoublyLinkedList();
